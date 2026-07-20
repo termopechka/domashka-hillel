a9be62eae5ed1c0c655b0963bf4e209bd1bebd76
archrog <kanditalex@gmail.com>
2026-07-21 00:27:15 +0200

    Fixed verify and refresh API endpoints for 28th homework

--- dz_10/BookShop/urls.py
+++ dz_10/BookShop/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import IndexView
 
@@ -29,6 +29,8 @@ api_urlpatterns = [
     path('orders/', include(('orders.api', 'orders'), namespace='orders')),
     path('', include(('books.api', 'catalog'), namespace='catalog')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 web_urlpatterns = [
